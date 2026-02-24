922fa83dcf12e639bd2786a204f7642474ec40bc
HamimBhai742 <mdhamim5088@gmail.com>
2026-02-24 11:03:01 +0600

    fix jwt issue

--- src/app/modules/stripe/stripe.route.ts
+++ src/app/modules/stripe/stripe.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { subscriptionController } from "./stripe.controller";
 import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../interface/user.interface";
 
 const router = Router();
 
@@ -13,7 +14,7 @@ router.get("/get-subscription",
     subscriptionController.getAllPlansController);
 
 router.post("/purchase-subscription",
-   checkAuth(),
+   checkAuth(Role.USER),
     subscriptionController.purchaseSubscriptionController);
     
 router.post("/un-subscription",
--- src/app/utils/verifyToken.ts
+++ src/app/utils/verifyToken.ts
@@ -1,4 +1,16 @@
-import jwt, { Secret } from 'jsonwebtoken'
-export const verifyToken=async(token: string,secret:Secret)=>{
-    return jwt.verify(token,secret)
-}
+import { JwtPayload, Secret } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import httpStatus from "http-status";
+import { AppError } from "../error/AppError";
+
+export const verifyToken = (token: string, secret: Secret) => {
+  try {
+    const decoded = jwt.verify(token, secret)  as JwtPayload;;
+    return decoded;
+  } catch (error: any) {
+    if (error instanceof jwt.TokenExpiredError) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'Token has expired!');
+    }
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Invalid token!');
+  }
+};

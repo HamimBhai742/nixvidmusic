e870e75901ea011146d6dc85d875120c6728697a
HamimBhai742 <mdhamim5088@gmail.com>
2026-03-03 09:16:26 +0600

    Updte profile api added

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,14 +1,48 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
 import { validateRequest } from "../../middleware/validationRequest";
-import { resendOtpSchema, resetPasswordSchema, userZodSchema, verifyOtpSchema, verifyRegisterOTPSchema } from "./user.zod.schema";
+import {
+  resendOtpSchema,
+  resetPasswordSchema,
+  userZodSchema,
+  verifyOtpSchema,
+  verifyRegisterOTPSchema,
+} from "./user.zod.schema";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../interface/user.interface";
 
-const router=Router()
+const router = Router();
 
-router.post('/sign-up',validateRequest(userZodSchema),userController.register)
-router.post('/verify-register-otp',validateRequest(verifyRegisterOTPSchema),userController.verifyRegisterOTP)
-router.post('/verify-forget-otp',validateRequest(verifyOtpSchema),userController.verifyForgetPasswordOtp)
-router.post('/forgot-password',validateRequest(resendOtpSchema),userController.requestPasswordReset)
-router.post('/reset-password',validateRequest(resetPasswordSchema),userController.resetPassword)
+router.post(
+  "/sign-up",
+  validateRequest(userZodSchema),
+  userController.register,
+);
+router.post(
+  "/verify-register-otp",
+  validateRequest(verifyRegisterOTPSchema),
+  userController.verifyRegisterOTP,
+);
+router.post(
+  "/verify-forget-otp",
+  validateRequest(verifyOtpSchema),
+  userController.verifyForgetPasswordOtp,
+);
+router.post(
+  "/forgot-password",
+  validateRequest(resendOtpSchema),
+  userController.requestPasswordReset,
+);
+router.post(
+  "/reset-password",
+  validateRequest(resetPasswordSchema),
+  userController.resetPassword,
+);
+router.post(
+  "/update-profile",
+  checkAuth(Role.USER),
+  userController.updateUserProfile,
+);
+router.post("/resend-register-otp", userController.resendRegisterOTP);
 
-export const userRoutes=router
+export const userRoutes = router;
